a5403fa5f30f241984ce7103c1ce74a4c4f09948
Delwin Kim <DelwinKim314@gmail.com>
2025-01-11 01:36:04 -0600

    Changed db to PostgreSQL and fixed dependencies

--- BootstrapAgrilife/settings.py
+++ BootstrapAgrilife/settings.py
@@ -84,9 +84,16 @@ WSGI_APPLICATION = 'BootstrapAgrilife.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DATABASE_URL')  # Use DATABASE_URL from environment
+        default=config('DATABASE_URL'),  # Use DATABASE_URL from environment
+        conn_max_age=600,
     )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
