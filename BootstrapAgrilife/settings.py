690456d76782ccb131e23576f89c098267ecafd2
Delwin Kim <DelwinKim314@gmail.com>
2025-01-12 11:36:32 -0600

    Trying to allow all hosts

--- BootstrapAgrilife/settings.py
+++ BootstrapAgrilife/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['agricultural-dashboard.onrender.com/', 'localhost', '127.0.0.1']
-
+# ALLOWED_HOSTS = ['agricultural-dashboard.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
